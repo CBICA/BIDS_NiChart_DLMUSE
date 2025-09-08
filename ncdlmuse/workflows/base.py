aadd22c7c72357a95cc52e0b56deac8d01401d09
Tien Tong <tien.tong98@gmail.com>
2025-09-08 09:15:56 -0400

    add --refaced-data cli

--- ncdlmuse/workflows/base.py
+++ ncdlmuse/workflows/base.py
@@ -84,6 +84,7 @@ def init_ncdlmuse_wf(name='ncdlmuse_wf'):
     disable_tta = config.workflow.dlmuse_disable_tta
     clear_cache = config.workflow.dlmuse_clear_cache
     save_all_outputs = config.workflow.dlmuse_save_all_outputs
+    refaced_data = config.workflow.dlmuse_refaced_data
 
     # --- Basic Workflow Setup --- #
     workflow = Workflow(name=name)
@@ -247,6 +248,7 @@ def init_ncdlmuse_wf(name='ncdlmuse_wf'):
                     disable_tta=disable_tta,
                     clear_cache=clear_cache,
                     save_all_outputs=save_all_outputs,
+                    refaced_data=refaced_data,
                     name=f'single_subject_{node_prefix}_wf',
                 )
                 workflow.add_nodes([subject_wf])
@@ -284,6 +286,7 @@ def init_single_subject_wf(
     disable_tta=False,
     clear_cache=False,
     save_all_outputs=False,
+    refaced_data=False,
     name='single_subject_wf',
 ):
     """Initialize the NCDLMUSE processing pipeline for a single subject/session T1w.
@@ -495,6 +498,7 @@ BIDS_NiChart_DLMUSE is built using *Nipype* version {config.environment.nipype_v
             disable_tta=disable_tta,
             clear_cache=clear_cache,
             save_all_outputs=save_all_outputs,
+            refaced_data=refaced_data,
             **(({'model_folder': model_folder}) if model_folder else {}),
             **(
                 ({'derived_roi_mappings_file': derived_roi_mappings_file})
